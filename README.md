622227717ba18ee68409fd21b2a789f6baaeb560
Kunal Gupta <kunal.gupta@myself.com>
2018-02-05 03:10:49 +0530

    Added Docs.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Neural-Network-from-scratch
+
+`neural_network.py` contains the source code along with the required docs.
+
+For more activation functions, simpley inherit the `ActivationFunction` class from the `functions.py` file.
--- functions.py
+++ functions.py
@@ -3,25 +3,44 @@ from abc import abstractmethod, ABC
 
 
 class ActivationFunction:
-
+    '''
+    Standard Activation Function Classs for the Neurons.
+    '''
     @staticmethod
     @abstractmethod
     def f(x):
+        '''
+        Returns the value of the function at input parameter x
+        :param x: Input Parameter
+        :return: Function Value
+        '''
         pass
 
     @staticmethod
     @abstractmethod
     def fprime_y(y):
+        '''
+        Returns the derivative of the function in terms of the function value.
+        :param y: Function value
+        :return: Derivative
+        '''
         pass
 
     @staticmethod
     @abstractmethod
     def fprime(x):
+        '''
+        Returns the derivative of the function in terms of the input value to the function.
+        :param x: Input Value
+        :return: Derivative
+        '''
         pass
 
 
 class Sigmoid(ActivationFunction):
-
+    '''
+    The standard sigmoid function : 1/(1 + exp(-x))
+    '''
     def f(x):
         return 1 / (1 + np.exp(-x))
 
